ba2a793494575181b4139e46dfd89a1eeff55b1e
Matteo Visentin <visentinmatt@gmail.com>
2023-08-12 12:01:01 +0200

    Added prop-types

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes, { number } from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div 
@@ -8,4 +10,21 @@ export const MovieCard = ({ movie, onMovieClick }) => {
             {movie.title}
         </div>
     );
+};
+
+MovieCard.PropTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        genre: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+        }),
+        director: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            bio: PropTypes.string.isRequired,
+            birthyear: PropTypes.number
+        }),
+        description: PropTypes.string.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -6,7 +6,14 @@ export const MovieView = ({ movie, onBackClick }) => {
                 <span>Title: </span>
                 <span>{movie.title}</span>
             </div>
-
+            <div>
+                <span>Director: </span>
+                <span>{movie.director.name}</span>
+            </div>
+            <div>
+                <span>Genre: </span>
+                <span>{movie.genre.name}</span>
+            </div>
             <div>
                 <span>Description: </span>
                 <span>{movie.description}</span>
