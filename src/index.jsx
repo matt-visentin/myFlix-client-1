6de708ce2901c06c6434fa9dea9b56278c947d80
Matteo Visentin <visentinmatt@gmail.com>
2023-08-15 20:27:01 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,21 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from "./components/main-view/main-view";
+import Container from "react-bootstrap/Container";
 
 // Import statment of bootstrap-library
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
+import { Container } from 'react-bootstrap';
 
 // Main component
 const MyFlixApp = () => {
-  return <MainView />;
+  return (
+    <Container style={{border: "1px solid red"}}>
+      <MainView />
+    </Container>
+  );
 };
 
 // Finds the root of your app
