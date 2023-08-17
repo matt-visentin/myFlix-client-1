3b55add7e89b91a3216dcf69871aa38f38fa1cee
Matteo Visentin <visentinmatt@gmail.com>
2023-08-17 22:23:31 +0200

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,32 +1,40 @@
 import PropTypes from "prop-types";
+import { Row, Card, Col, Container, Card, CardGroup } from "react-bootstrap";
+
+import "./movie-view.scss";
 
 export const MovieView = ({ movie, onBackClick }) => {
     console.log(movie);
     return (
-        <div>
-            <div>
-            <img src={movie.ImagePath} className="w-100" alt="Movie Cover" />
-            </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.Title}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.Director.Name}</span>
-            </div>
-            <div>
-                <span>Genre: </span>
-                <span>{movie.Genre.Name}</span>
-            </div>
-            <div>
-                <span>Description: </span>
-                <span>{movie.Description}</span>
-            </div>
-            <div>
-                <button onClick={onBackClick}>Back</button>
-            </div>
-        </div>
+
+                    <CardGroup>
+                        <Card>
+                            <Card.Title>{movie.Title}</Card.Title>
+                            <Card.Body>
+                            <div>
+                                <div>
+                                <img src={movie.ImagePath} className="mw-100" alt="Movie Cover" />
+                                </div>
+                                <div>
+                                    <span>Director: </span>
+                                    <span>{movie.Director.Name}</span>
+                                </div>
+                                <div>
+                                    <span>Genre: </span>
+                                    <span>{movie.Genre.Name}</span>
+                                </div>
+                                <div>
+                                    <span>Description: </span>
+                                    <span>{movie.Description}</span>
+                                </div>
+                                <div>
+                                    <button onClick={onBackClick}>Back</button>
+                                </div>
+                            </div>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+
     );
 };
 
